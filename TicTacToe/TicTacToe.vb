04827ced7876e9fccae5f5c8fc43f34bfada158f
Elliot Heiner <elliotheiner1997@gmail.com>
2021-11-29 19:07:08 -0700

    Convert user input coordinates to values that can be associated with an array.

--- TicTacToe/TicTacToe.vb
+++ TicTacToe/TicTacToe.vb
@@ -4,7 +4,8 @@
 
 Option Strict On
 Option Explicit On
-Option Compare Binary
+'Option Compare Binary
+Option Compare Text
 
 'TODO:
 'display game board console
@@ -19,7 +20,41 @@ Option Compare Binary
 
 Module TicTacToe
 
+    Dim gameBoard(2, 2) As String
+
+
     Sub Main()
+        Dim row As String = "3"
+        Dim rowCheck As String
+        Dim column As String
+        Dim userInput As String
+
+        Do Until CInt(row) <= 2
+            userInput = Console.ReadLine()
+            rowCheck = userInput.Chars(0)
+            column = userInput.Chars(1)
+            If rowCheck = "A" Then
+                row = "0"
+            ElseIf rowCheck = "B" Then
+                row = "1"
+            ElseIf rowCheck = "C" Then
+                row = "2"
+            Else
+                row = "3"
+            End If
+
+            If CInt(column) > 2 Then
+                row = "3"
+            End If
+
+            If row = "3" Then
+                MsgBox("test")
+            End If
+
+        Loop
+
+        Console.WriteLine(row & column)
+        Console.ReadLine()
 
     End Sub
 
